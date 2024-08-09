45b1750a86f11748c3fc239937ebe1f1b4b8a1b7
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-08-10 00:57:19 +0530

    add config setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,21 @@
 /* eslint-env node */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "prettier",
+    'eslint:recommended', 
+    'plugin:@typescript-eslint/recommended-type-checked', 
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    projectService: true,
+    project: true,
     tsconfigRootDir: __dirname,
   },
   root: true,
   rules: {
-    "no-console": "error",
-    "dot-notation": "error",
-  },
+    "no-console": "off",
+    "dot-notation": "error"
+  }
 };
+
+
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-function login(username: string): string {
-  const user = {
-    name: "nikhil",
-  };
-  const name = user.name;
-  return username + name;
-}
-
-login("nikhil");
