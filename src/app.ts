54dc72cbbc39507ce87f75670f976b6ee4217b5c
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-11-11 17:42:34 +0530

    add utility classess

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import { globalErrorHandler } from "./middlewares/globalErrrorHandler";
 
 const app = express();
-
-
 
 app.use(globalErrorHandler);
 
